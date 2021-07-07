f492fd26fc483f68db2fc0b62da8a04d4d180051
Fritzchips <fritzjohn_solibio@yahoo.ca>
2021-07-06 21:46:48 -0700

    refactoring part 2

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 const time = document.getElementById("time");
 
 let swap = true;
-
 let goGo;
 let clock = false;
 
-function run() {
+const run = () => {
   goGo = setInterval(countDown, 1000);
   clock = true;
-}
+};
 
-function halt() {
+const halt = () => {
   clearInterval(goGo);
   clock = false;
-}
+};
 
 const beep = document.querySelector("audio");
-function alarm() {
+const alarm = () => {
   beep.play();
-}
+  beep.loop = true;
+};
 
-function stopAlarm() {
+const stopAlarm = () => {
   beep.pause();
   !swap ? working() : resting();
-}
+};
 
 let seconds = 0;
 let minutes = 25;
 const appendLeadingZero = (value) => (value < 10 ? `0${value}` : value);
-function countDown() {
+const countDown = () => {
   if (minutes === 0 && seconds === 1) {
     stopBtn.textContent = "Stop";
     halt();
@@ -41,23 +41,23 @@ function countDown() {
   seconds--;
 
   time.textContent = `${minutes}:${appendLeadingZero(seconds)}`;
-}
+};
 
-function working() {
+const working = () => {
   minutes = 25;
   seconds = 0;
   time.textContent = `${minutes}:${appendLeadingZero(seconds)}`;
   swap = true;
   halt();
-}
+};
 
-function resting() {
-  minutes = 0;
-  seconds = 10;
+const resting = () => {
+  minutes = 5;
+  seconds = 0;
   time.textContent = `${minutes}:${appendLeadingZero(seconds)}`;
   swap = false;
   halt();
-}
+};
 
 const work = document.getElementById("work");
 work.addEventListener("click", () => working());
