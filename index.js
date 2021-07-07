cd19e253a208071947e84f2fe93764a3dd885b96
Fritzchips <fritzjohn_solibio@yahoo.ca>
2021-07-06 21:29:10 -0700

    refactored code

--- index.js
+++ index.js
@@ -1,137 +1,88 @@
-let work = document.getElementById('work');
-let rest = document.getElementById('rest');
-let time = document.getElementById('time');
-let start = document.getElementById('start');
-let stopBtn = document.getElementById('stop');
-let reset = document.getElementById('reset');
-let beep = document.querySelector('audio');
-
-let seconds = 0;
-let tenthSeconds = 0;
-let minutes = 25; 
-
+const time = document.getElementById("time");
 
 let swap = true;
-
 
 let goGo;
 let clock = false;
 
-
 function run() {
-    goGo = setInterval(countDown, 1000);
-  
-
-    clock = true;
+  goGo = setInterval(countDown, 1000);
+  clock = true;
 }
 
 function halt() {
-    clearInterval(goGo);
-    clock = false;
+  clearInterval(goGo);
+  clock = false;
 }
 
+const beep = document.querySelector("audio");
 function alarm() {
-    beep.play();
-} 
+  beep.play();
+}
 
 function stopAlarm() {
-    beep.pause();
+  beep.pause();
+  !swap ? working() : resting();
+}
 
-    if(swap === false) {
-        working();  
-    } else {
-        resting();
-    }
-    
-} 
-
+let seconds = 0;
+let minutes = 25;
+const appendLeadingZero = (value) => (value < 10 ? `0${value}` : value);
 function countDown() {
-    if(seconds == 0) {
-        moveTime();
-        time.innerText =  minutes + ':' + tenthSeconds + seconds;
-    } else if (seconds == 1) {
-        seconds = 0;
-        time.innerText = minutes + ':' + tenthSeconds + seconds;
-    } else {
-        seconds --;
-        time.innerText = minutes + ':' + tenthSeconds + seconds;
-    }
-} 
+  if (minutes === 0 && seconds === 1) {
+    stopBtn.textContent = "Stop";
+    halt();
+    alarm();
+  }
+  if (seconds === 0) {
+    seconds = 60;
+    minutes--;
+  }
+  seconds--;
 
-function moveTime() {
-    if( tenthSeconds == 0 && minutes == 0 && seconds == 0) {
-        stopBtn.innerText = "Stop";
-        alarm();
-
-    } else if(seconds == 0){
-
-        if(tenthSeconds == 0 && minutes == 0){
-            tenthSeconds = 0;
-        } else if(tenthSeconds == 0){
-            tenthSeconds = 5;
-        } else {
-           tenthSeconds --; 
-        }
-        
-        if(tenthSeconds == 5){
-            if(minutes > 0){
-               minutes--; 
-            } else {
-                minutes = 0;
-            }
-        }
-
-        seconds = 9;
-    }
+  time.textContent = `${minutes}:${appendLeadingZero(seconds)}`;
 }
 
 function working() {
-    time.innerText = "25:00";
-    seconds = 0;
-    tenthSeconds = 0;
-    minutes = 25;
-    swap = true;
-    halt();
+  minutes = 25;
+  seconds = 0;
+  time.textContent = `${minutes}:${appendLeadingZero(seconds)}`;
+  swap = true;
+  halt();
 }
 
 function resting() {
-    time.innerText = "5:00";
-    seconds = 0;
-    tenthSeconds = 0;
-    minutes = 5;
-    swap = false;
+  minutes = 0;
+  seconds = 10;
+  time.textContent = `${minutes}:${appendLeadingZero(seconds)}`;
+  swap = false;
+  halt();
+}
+
+const work = document.getElementById("work");
+work.addEventListener("click", () => working());
+
+const rest = document.getElementById("rest");
+rest.addEventListener("click", () => resting());
+
+const stopBtn = document.getElementById("stop");
+stopBtn.addEventListener("click", () => {
+  if (minutes === 0 && seconds === 0) {
+    stopAlarm();
+    stopBtn.textContent = "Pause";
+  } else if (clock === true) {
     halt();
-} 
+  }
+});
 
-work.addEventListener('click', () => {
-    working();
-})
+const reset = document.getElementById("reset");
+reset.addEventListener("click", () => {
+  swap ? working() : resting();
+});
 
-rest.addEventListener('click', () => {
-    resting();
-})
-
-stopBtn.addEventListener('click', () => {
-    if( tenthSeconds == 0 && minutes == 0 && seconds == 0) {
-        stopAlarm();
-        stopBtn.innerText = "Pause";
-    } else if (clock === true) {
-        halt();
-    }
-    
-})
-
-reset.addEventListener('click', () => {
-    if(swap === true) {
-        working();
-    } else {
-        resting();
-    }
-})
-
-start.addEventListener('click', ()=> {
-    if(clock === false) {
-        run();
-    }
-})
-
+const start = document.getElementById("start");
+start.addEventListener("click", () => {
+  if (clock === false) {
+    run();
+  }
+});
